279193a480f57030751d8c662066008aabdad0da
Collin Poczatek <collin.poczatek@gmail.com>
2018-08-13 15:11:35 -0700

    point at scripts v7.1.0, remove some valid versions

--- extensions/node.sh
+++ extensions/node.sh
@@ -34,7 +34,7 @@ mkdir -p /data/cassandra/saved_caches
 useradd cassandra
 chown -R cassandra:cassandra /data/cassandra
 
-release="dev"
+release="7.1.0"
 wget https://github.com/DSPN/install-datastax-ubuntu/archive/$release.tar.gz
 tar -xvf $release.tar.gz
 
